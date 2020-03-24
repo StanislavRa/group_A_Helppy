5804fae55e6e0e0618b7c111c43edb08c9d3a4ae
Demi Avaliani <demikoavaliani@gmail.com>
2020-03-24 18:47:48 +0200

    Added AdDetailsView.FXML + controller

---
+++ src/main/java/controller/AdDetailsViewController.java
@@ -0,0 +1,77 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class AdDetailsViewController {
+
+    @FXML
+    private Text logoText;
+
+    @FXML
+    private Label categoryLabel;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Button myAdsButton;
+
+    @FXML
+    private Text addressText;
+
+    @FXML
+    private Text categoryText;
+
+    @FXML
+    private Text endDateText;
+
+    @FXML
+    private Button allAdsButton;
+
+    @FXML
+    private Text adTypeText;
+
+    @FXML
+    private Text startDateText;
+
+    @FXML
+    private Label startDateLabel;
+
+    @FXML
+    private Label endDateLabel;
+
+    @FXML
+    private Label subjectLabel;
+
+    @FXML
+    private Label adTypeLabel;
+
+    @FXML
+    private Text priceText;
+
+    @FXML
+    private Label addressLable;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    void logoTextPushed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void myAdsButtonPushed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void allAdsButtonPushed(ActionEvent event) {
+
+    }
+
+}
