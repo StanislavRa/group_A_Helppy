526a5e6652d2bd19252d5efcd0bb2f82aa42effd
Demi Avaliani <demikoavaliani@gmail.com>
2020-03-31 01:26:40 +0300

    1) Added seeDetailsButtonPushed() method to AllAdsViewController 2) Added innitData() method to AdDetailsViewController

--- src/main/java/com/sda/controller/AdDetailsViewController.java
+++ src/main/java/com/sda/controller/AdDetailsViewController.java
@@ -1,5 +1,7 @@
 package com.sda.controller;
 
+import com.sda.dao.implementation.AdvertisementDao;
+import com.sda.entity.Advertisement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -9,10 +11,13 @@ import javafx.scene.control.Label;
 public class AdDetailsViewController extends GeneralController {
 
     @FXML
-    private Label startDateLabel;
+    private Label subjectLabel;
 
     @FXML
     private Label categoryLabel;
+
+    @FXML
+    private Label startDateLabel;
 
     @FXML
     private Label endDateLabel;
@@ -21,17 +26,26 @@ public class AdDetailsViewController extends GeneralController {
     private Label addressLabel;
 
     @FXML
-    private Label subjectLabel;
-
-    @FXML
     private Label adTypeLabel;
-
-    @FXML
-    private Label userFullNameLabel;
 
     @FXML
     private Label priceLabel;
 
     @FXML
     private Label descriptionLabel;
+
+    @FXML
+    private Label userFullNameLabel;
+
+    public void initData(Advertisement advertisement)  {
+        subjectLabel.setText(advertisement.getSubject());
+        categoryLabel.setText(advertisement.getCategory().toString());
+        startDateLabel.setText(advertisement.getStartDate().toString());
+        endDateLabel.setText(advertisement.getEndDate().toString());
+        addressLabel.setText(advertisement.getAddress().toString());
+        adTypeLabel.setText(advertisement.getServiceType().toString());
+        priceLabel.setText(String.valueOf(advertisement.getPrice()));
+        descriptionLabel.setText(advertisement.getDescription());
+        userFullNameLabel.setText(advertisement.getCustomer().getFullName());
+    }
 }
--- src/main/java/com/sda/controller/AllAdsViewController.java
+++ src/main/java/com/sda/controller/AllAdsViewController.java
@@ -1,12 +1,24 @@
 package com.sda.controller;
 
+import com.sda.entity.Address;
+import com.sda.entity.Advertisement;
+import com.sda.entity.Category;
+import com.sda.entity.Customer;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author StanislavR
  */
 public class AllAdsViewController extends GeneralController {
+
+    @FXML
+    private Button seeDetailsButton;
 
     @FXML
     private ToggleGroup radioButtonToggleGroup;
@@ -65,5 +77,45 @@ public class AllAdsViewController extends GeneralController {
     @FXML
     private RadioButton locationRadioButton;
 
+    @FXML
+    void seeDetailsButtonPushed(ActionEvent event) throws ParseException {
+        // create new customer
+        Customer customer = new Customer();
+        customer.setLogin("Pjotr");
+        customer.setPassword("123456");
+        customer.setFullName("Petr III");
+
+        // create new address
+        Address addressTest1 = new Address("Estonia", "Tallinn", "Parnu mnt");
+
+        // create new advertisement
+        String startDateString1 = "31/12/1998";
+        Date startDate1 = new SimpleDateFormat("dd/MM/yyyy").parse(startDateString1);
+
+        String endDateString1 = "31/12/1998";
+        Date endDate1 = new SimpleDateFormat("dd/MM/yyyy").parse(endDateString1);
+
+        Advertisement advertisement1 = new Advertisement(
+                "Clean Fast",
+                "blablabla",
+                "2.5",
+                startDate1,
+                endDate1,
+                "OFFER",
+                new Category("CLEANING"),
+                customer);
+        advertisement1.setServiceState(Advertisement.ServiceState.INACTIVE);
+
+        // assign address to advertisement
+        advertisement1.setAddress(addressTest1);
+
+        // assign customer to advertisement
+        advertisement1.setCustomer(customer);
+
+        AdDetailsViewController controller = (changeScreen(event, "/views/adDetailsView.fxml").getController());
+
+        controller.setCustomer(customer);
+        controller.initData(advertisement1);
+    }
 
 }
