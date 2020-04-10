5bbb917144e17e654ea90637bcc16b66a259e993
Demi Avaliani <demikoavaliani@gmail.com>
2020-04-10 21:19:56 +0300

    1) Added initData() method body

--- src/main/java/com/sda/controller/AdDetailsViewController.java
+++ src/main/java/com/sda/controller/AdDetailsViewController.java
@@ -1,7 +1,11 @@
 package com.sda.controller;
 
+import com.sda.entity.Advertisement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author StanislavR
@@ -34,4 +38,23 @@ public class AdDetailsViewController extends GeneralController {
 
     @FXML
     private Label descriptionLabel;
+
+    public void initData(Advertisement advertisement)  {
+        subjectLabel.setText(advertisement.getSubject());
+        categoryLabel.setText(advertisement.getCategory().getName());
+        startDateLabel.setText(dateParser(advertisement.getStartDate()));
+        endDateLabel.setText(dateParser(advertisement.getEndDate()));
+        addressLabel.setText(advertisement.getAddress().getCity());
+        adTypeLabel.setText(advertisement.getServiceType().toString());
+        priceLabel.setText(advertisement.getPrice().toString());
+        descriptionLabel.setText(advertisement.getDescription());
+        userFullNameLabel.setText(advertisement.getCustomer().getFullName());
+    }
+
+    public String dateParser(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+        String formattedDate = formatter.format(date);
+        return formattedDate;
+    }
 }
+
