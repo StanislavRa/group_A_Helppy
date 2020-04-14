47f183e63aaa3b90644c087e9bca163a45614400
StanislavRa <s.ratsinski@hotmail.com>
2020-04-15 00:33:31 +0300

    Restore myAdsView.fxml and MyAdsController to the master

---
+++ src/main/java/com/sda/controller/MyAdsController.java
@@ -0,0 +1,212 @@
+package com.sda.controller;
+
+import com.sda.controller.utilities.AlertBox;
+import com.sda.controller.utilities.Validator;
+import com.sda.entity.Address;
+import com.sda.entity.Advertisement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MyAdsController extends TableSetUp implements Initializable {
+
+    @FXML
+    private ComboBox<String> categoryComboBox;
+    @FXML
+    private DatePicker startDatePicker;
+    @FXML
+    private DatePicker endDatePicker;
+    @FXML
+    private TextField priceTextField;
+    @FXML
+    private ComboBox<String> countryComboBox;
+    @FXML
+    private ComboBox<String> cityComboBox;
+    @FXML
+    private RadioButton offerServiceRadioButton;
+    @FXML
+    private RadioButton requestServiceRadioButton;
+    @FXML
+    private TextField subjectTextField;
+    @FXML
+    private TextArea descriptionTextField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        setUpTableColumns();
+        populateFieldsFromSelectedRow();
+    }
+
+    protected void initData() {
+
+        List<Advertisement> getUserAds = customer.getUserAdvertisements();
+        ObservableList<Advertisement> adsTable = FXCollections.observableArrayList(getUserAds);
+
+        mainTableView.setItems(adsTable);
+        countryComboBox.getItems().addAll(countryDao.getAllCountriesNamesList(countryDao.getAll()));
+        categoryComboBox.getItems().addAll((categoryDao.getAllCategoriesList()));
+    }
+
+    @FXML
+    private void countryComboBoxSelected(javafx.event.ActionEvent event) {
+        cityComboBox.getItems().clear();
+        List<String> listOfCityNames = cityDao.getAllCitiesByCountryList(countryComboBox.getValue());
+
+        cityComboBox.getItems().addAll(listOfCityNames);
+    }
+
+    private boolean userInputNullCheck() {
+        if (
+                categoryComboBox.getSelectionModel().isEmpty() ||
+                        startDatePicker.getValue() == null ||
+                        endDatePicker.getValue() == null ||
+                        priceTextField.getText().isEmpty() ||
+                        countryComboBox.getSelectionModel().isEmpty() ||
+                        cityComboBox.getSelectionModel().isEmpty() ||
+                        subjectTextField.getText().isEmpty() ||
+                        descriptionTextField.getText().isEmpty() &&
+                                (offerServiceRadioButton == null || requestServiceRadioButton == null)
+        ) {
+            AlertBox.validation("Make sure you fill all necessary data");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean fieldsValidator() {
+
+        return userInputNullCheck() &&
+                Validator.startDateIsBeforeEndDate(
+                        startDatePicker.getValue(), startDatePicker.getValue(),
+                        "Make sure that End Date is after Start Date") &&
+                Validator.dateIsNotPast(
+                        endDatePicker.getValue(),
+                        "Make sure that you don't choose End Date from past") &&
+                Validator.stringMatcherValidation
+                        (priceTextField.getText(), "[0-9]*['.']?[0-9]*",
+                                "Make sure that price number format is xx.xx");
+    }
+
+    private Advertisement.ServiceType serviceTypeSelected() {
+        return offerServiceRadioButton.isSelected() ?
+                Advertisement.ServiceType.OFFER : Advertisement.ServiceType.REQUEST;
+    }
+
+    private Advertisement getAdFromFields() {
+
+        return new Advertisement(
+                subjectTextField.getText(),
+                descriptionTextField.getText(),
+                priceTextField.getText(),
+                parser.convertToDateViaSqlDate(startDatePicker.getValue()),
+                parser.convertToDateViaSqlDate(endDatePicker.getValue()),
+                serviceTypeSelected(),
+                categoryDao.getByName(categoryComboBox.getValue()),
+                customer,
+                new Address(countryComboBox.getValue(), cityComboBox.getValue()));
+    }
+
+    private Advertisement changeAdFromFields(Advertisement advertisement) {
+
+        advertisement.setSubject(subjectTextField.getText());
+        advertisement.setDescription(descriptionTextField.getText());
+        advertisement.setPrice(priceTextField.getText());
+        advertisement.setStartDate(parser.convertToDateViaSqlDate(startDatePicker.getValue()));
+        advertisement.setEndDate(parser.convertToDateViaSqlDate(endDatePicker.getValue()));
+        advertisement.setServiceType(serviceTypeSelected());
+        advertisement.setCategory(categoryDao.getByName(categoryComboBox.getValue()));
+        advertisement.setAddress(new Address(countryComboBox.getValue(), cityComboBox.getValue()));
+
+        return advertisement;
+    }
+
+    @FXML
+    void createButtonPushed() {
+
+        if (fieldsValidator()) {
+
+            Advertisement advertisement = getAdFromFields();
+            adDao.save(advertisement);
+            updateCustomer();
+            initData();
+            AlertBox.success("New ad " + advertisement.getSubject() + " created");
+            clearValues();
+        }
+    }
+
+    @FXML
+    void updateButtonPushed() {
+
+        if (fieldsValidator()) {
+
+            Advertisement shouldUpdateThisAd = adDao.get(getFromSelectedRow(mainTableView).getId());
+            Advertisement newAd = changeAdFromFields(shouldUpdateThisAd);
+            adDao.update(newAd);
+            updateCustomer();
+            initData();
+            AlertBox.success(newAd.getSubject() + " ad updated");
+            clearValues();
+        }
+    }
+
+    @FXML
+    void deleteButtonPushed() {
+
+        ObservableList<Advertisement> allAds = getAllFromTable(mainTableView);
+        ObservableList<Advertisement> selectedRows = getSelectedFromTable(mainTableView);
+
+        for (Advertisement advertisement : selectedRows) {
+
+            if (AlertBox.confirmation(advertisement.getSubject(), "Are you sure you want to delete")) {
+
+                allAds.remove(advertisement);
+                Advertisement shouldDeleteThisAd = adDao.get(advertisement.getId());
+                adDao.delete(shouldDeleteThisAd);
+                updateCustomer();
+            }
+        }
+    }
+
+    @FXML
+    void clearValues() {
+
+        categoryComboBox.getSelectionModel().clearSelection();
+        startDatePicker.getEditor().clear();
+        endDatePicker.getEditor().clear();
+        priceTextField.clear();
+        countryComboBox.getSelectionModel().clearSelection();
+        offerServiceRadioButton.setSelected(false);
+        requestServiceRadioButton.setSelected(false);
+        subjectTextField.clear();
+        descriptionTextField.clear();
+    }
+
+    public void populateFieldsFromSelectedRow() {
+
+        mainTableView.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+
+                subjectTextField.setText(getFromSelectedRow(mainTableView).getSubject());
+                descriptionTextField.setText(getFromSelectedRow(mainTableView).getDescription());
+                priceTextField.setText(getFromSelectedRow(mainTableView).getPrice().toString());
+                countryComboBox.setValue(getFromSelectedRow(mainTableView).getAddress().getCountry());
+                cityComboBox.setValue(getFromSelectedRow(mainTableView).getAddress().getCity());
+                categoryComboBox.setValue(getFromSelectedRow(mainTableView).getCategory().getName());
+                endDatePicker.setValue(parser.convertToLocalDateViaInstant(getFromSelectedRow(mainTableView).getEndDate()));
+                startDatePicker.setValue(parser.convertToLocalDateViaInstant(getFromSelectedRow(mainTableView).getStartDate()));
+                if (getFromSelectedRow(mainTableView).getServiceType() == Advertisement.ServiceType.OFFER) {
+                    offerServiceRadioButton.setSelected(true);
+                } else requestServiceRadioButton.setSelected(true);
+            }
+        });
+    }
+}
