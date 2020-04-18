cb50e2e7b306d5ca728f20b06ad5934e3daf13a4
Mariam Dgebuadze <57587423+Dgebu@users.noreply.github.com>
2020-04-18 09:20:24 +0300

    Adding ValidatorTest as a file
    For some reason I couldn't get latest updatest to my brabch (will try to resolve later), so, I'll just add test file for Validator class directly.
    Not sure whether the exception type is correct - we can discuss it later today

---
+++ src/test/java/com/sda/controller/utilities/ValidatorTest.java
@@ -0,0 +1,44 @@
+package com.sda.controller.utilities;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+
+public class ValidatorTest {
+
+    Validator validator;
+    @Before
+    public void setUp() {
+        validator = new Validator();
+    }
+
+
+    @Test
+    public void endDateShouldNotBeLessThanStartDate() throws DateTimeException {
+        LocalDate startDate = LocalDate.of(2019, 10, 8);
+        LocalDate endDate = LocalDate.of(2019, 11, 6);
+        String message ="testing date validator";
+        boolean result = validator.startDateIsBeforeEndDate(startDate, endDate, message);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+
+    public void dateShouldNotBePast() throws DateTimeException {
+        LocalDate startDate = LocalDate.of(2019, 10, 8);
+        LocalDate endDate = LocalDate.of(2019, 11, 6);
+        String message ="testing date validator";
+        boolean result1 = validator.dateIsNotPast(startDate, message);
+        boolean result2 = validator.dateIsNotPast(endDate, message);
+
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+
+    }
+
+}
